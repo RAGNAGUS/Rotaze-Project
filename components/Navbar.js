352215b3697ecd166a7b2f326a61f611e7a8042a
Kongsittha Nimking <casnova152@gmail.com>
2022-07-29 19:26:07 +0700

    create and design navbar and sidebar done!!

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,47 @@
+import { MdNotificationsNone } from 'react-icons/md'
+import { TbMessageCircle } from 'react-icons/tb'
+
 const Navbar = () => {
     return (
-        <nav className="fixed w-full h-16 bg-gray-800">
-            <div className="flex items-center justify-center w-full h-full">
-                <div>
+        <nav className="fixed w-full bg-white border-b border-gray-200">
+            <div className="px-4 mx-auto max-w-7xl">
+                <div className="flex items-center justify-between h-[73px] gap-x-2 md:gap-x-6">
+
+                    {/* logo text */}
+                    <a className="hidden px-2 py-1 duration-100 ease-in-out border-2 border-gray-500 rounded hover:border-gray-600 lg:inline-block" href="#">
+                        <span className='text-2xl font-semibold'>ROTAZE</span>
+                    </a>
+
+                    {/* Nav Links */}
+                    <ul className="hidden text-gray-700 md:flex md:gap-x-4 md:items-center">
+                        <li><a className="px-3 py-2 font-medium duration-150 ease-in-out rounded hover:bg-gray-600 hover:text-white" href="#">Home</a></li>
+                        <li><a className="px-3 py-2 font-medium duration-150 ease-in-out rounded hover:bg-gray-600 hover:text-white" href="#">Profile</a></li>
+                        <li><a className="px-3 py-2 font-medium duration-150 ease-in-out rounded hover:bg-gray-600 hover:text-white" href="#">Discover</a></li>
+                        <li><a className="px-3 py-2 font-medium duration-150 ease-in-out rounded bg-gradient-to-r from-[#de6161]  to-[#2657eb] text-white" href="#">Premium</a></li>
+                    </ul>
+
+                    {/* Search */}
+                    <input
+                        className="ml-10 md:ml-0 flex-1 md:flex-1 py-1.5 px-4 text-gray-700 bg-gray-100 rounded-full border-[2px] border-gray-100 transition focus:outline-none focus:bg-white focus:border-gray-600"
+                        type="text"
+                        placeholder="Search"
+                    />
+
+                    {/* upload notification and profile */}
+                    <div className="flex items-center gap-x-4 lg:flex-row">
+                        <button className="ml-3 md:ml-0 hidden sm:inline-block py-1.5 px-5 rounded-full border border-gray-200 hover:scale-110 duration-150 ease-in-out">
+                            Upload
+                        </button>
+                        <button className="ml-2 sm:ml-1 lg:inline-block">
+                            <TbMessageCircle className='w-7 h-7 invert-[40%] hover:invert-[30%]' />
+                        </button>
+                        <button className="lg:inline-block">
+                            <MdNotificationsNone className='w-7 h-7 invert-[40%] hover:invert-[30%]' />
+                        </button>
+                        <button className="object-cover w-10 h-10 overflow-hidden rounded-full lg:inline-block">
+                            <img src="https://picsum.photos/150" alt="" />
+                        </button>
+                    </div>
                 </div>
             </div>
         </nav>
--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -1,21 +1,18 @@
-import { GiHamburgerMenu } from "react-icons/gi";
-import {
-    MdOutlineSpaceDashboard,
-    MdOutlineAnalytics,
-    MdOutlineIntegrationInstructions,
-    MdOutlineMoreHoriz,
-    MdOutlineSettings,
-    MdOutlineLogout,
-} from "react-icons/md";
-import { CgProfile } from "react-icons/cg";
-import { FaRegComments } from "react-icons/fa";
-import { BiMessageSquareDots } from "react-icons/bi";
 import { useEffect, useState } from "react";
 
+// imports icons
+import Image from "next/image";
+import MenuIcon from "../public/menu.svg";
+import { BiHome } from 'react-icons/bi'
+import { MdLogout } from 'react-icons/md'
+import { CgProfile } from 'react-icons/cg'
+import { TbMessages } from 'react-icons/tb'
+import { IoSettingsOutline } from 'react-icons/io5'
+import { RiCompassDiscoverLine } from 'react-icons/ri'
 
 const SideNavbar = () => {
 
-    const [isPanelOpen, setIsPanelOpen] = useState(true)
+    const [isPanelOpen, setIsPanelOpen] = useState(false)
     const panelControl = () => {
         setIsPanelOpen(!isPanelOpen)
     }
@@ -40,70 +37,56 @@ const SideNavbar = () => {
 
     return (
         <div id="clickbox">
-            <button onClick={panelControl} className="fixed inline-flex items-center justify-center p-2 text-white rounded-md top-3 left-3 peer hover:bg-gray-900 group">
-                <GiHamburgerMenu className="block w-6 h-6" />
+            <button onClick={panelControl} className="fixed flex items-center justify-center p-2 rounded-md top-4 left-2 hover:bg-gray-100">
+                <div className="inline-block w-6 h-6 md:hidden invert-[30%]">
+                    <Image src={MenuIcon} alt="" />
+                </div>
             </button>
             <div>
-                <div className={`fixed bg-[#f9fafc] shadow-xl w-1/2 md:w-4/12 lg:w-1/6 h-screen top-16 p-6 duration-500 ease-in-out ${isPanelOpen ? 'left-0' : '-left-96'}`}>
+                <div className={`fixed bg-gray-800 shadow-xl w-1/2 md:w-4/12 lg:w-1/6 h-screen top-[74px] p-6 duration-500 ease-in-out ${isPanelOpen ? 'left-0' : '-left-96'}`}>
                     <div className="flex flex-col justify-start item-center">
+                        <div className="flex items-center justify-start px-5 py-3 m-auto mb-2 duration-150 ease-in-out rounded-md cursor-pointer bg-gradient-to-r from-[#de6161]  to-[#2657eb] group hover:shadow-lg hover:px-10 ">
+                            <h3 className="text-base font-semibold text-white ">
+                                Go Premium
+                            </h3>
+                        </div>
                         <div className="pb-4 my-4 border-b border-gray-100 ">
-                            <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <MdOutlineSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
-                                    Dashboard
+                            <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 bg-gray-700 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
+                                <BiHome className="text-2xl text-gray-300 group-hover:text-white" />
+                                <h3 className="text-base text-gray-300 font-semibolgray-300 group-hover:text-white ">
+                                    Home
                                 </h3>
                             </div>
                             <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <CgProfile className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
+                                <CgProfile className="text-2xl text-gray-300 group-hover:text-white " />
+                                <h3 className="text-base font-semibold text-gray-300 group-hover:text-white ">
                                     Profile
                                 </h3>
                             </div>
                             <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <FaRegComments className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
-                                    Comments
-                                </h3>
-                            </div>
-                            <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <MdOutlineAnalytics className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
-                                    Analytics
-                                </h3>
-                            </div>
-                            <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <BiMessageSquareDots className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
+                                <TbMessages className="text-2xl text-gray-300 group-hover:text-white " />
+                                <h3 className="text-base font-semibold text-gray-300 group-hover:text-white ">
                                     Messages
                                 </h3>
                             </div>
                             <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <MdOutlineIntegrationInstructions className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
-                                    Integration
+                                <RiCompassDiscoverLine className="text-2xl text-gray-300 group-hover:text-white " />
+                                <h3 className="text-base font-semibold text-gray-300 group-hover:text-white ">
+                                    Discover
                                 </h3>
                             </div>
-                        </div>
-                        {/* setting  */}
-                        <div className="pb-4 my-4 border-b border-gray-100 ">
                             <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <MdOutlineSettings className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
+                                <IoSettingsOutline className="text-2xl text-gray-300 group-hover:text-white " />
+                                <h3 className="text-base font-semibold text-gray-300 group-hover:text-white ">
                                     Settings
-                                </h3>
-                            </div>
-                            <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <MdOutlineMoreHoriz className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
-                                    More
                                 </h3>
                             </div>
                         </div>
                         {/* logout */}
                         <div className="my-4 ">
                             <div className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 border border-gray-200 rounded-md cursor-pointer hover:bg-gray-900 group hover:shadow-lg">
-                                <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base font-semibold text-gray-800 group-hover:text-white ">
+                                <MdLogout className="text-2xl text-gray-300 group-hover:text-white " />
+                                <h3 className="text-base font-semibold text-gray-300 group-hover:text-white ">
                                     Logout
                                 </h3>
                             </div>
