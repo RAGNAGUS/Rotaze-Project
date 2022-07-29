3fbe3db3305457a19ab7edd149afe0a850634c78
Kongsittha Nimking <casnova152@gmail.com>
2022-07-30 01:12:29 +0700

    add loading button in login page

--- pages/signup.js
+++ pages/signup.js
@@ -123,20 +123,32 @@ const Signup = () => {
                                 />
                             </label>
                             {passwordError && (
-                                <label className='flex items-center h-12 p-2 pl-4 text-gray-800 bg-red-100 border-2 border-red-500 rounded-md '>
+                                <label className='error-form'>
                                     {passwordError}
                                 </label>
                             )}
                             {errorFormat && (
-                                <label className='flex items-center h-12 p-2 pl-4 text-gray-800 bg-red-100 border-2 border-red-500 rounded-md '>
+                                <label className='error-form '>
                                     {errorFormat}
                                 </label>
                             )}
                             <div className="flex items-center justify-end w-full pt-2 space-x-3">
                                 <span className="text-sm text-right text-gray-800"><Link href="/login"><a>already have an account ?</a></Link></span>
-                                <button type='submit' className="h-10 text-white bg-gray-600 border rounded-md shadow-sm w-28">
-                                    <span>Sign up</span>
-                                </button>
+                                {!isPending && (
+                                    <button type='submit' className="h-10 text-white bg-gray-600 border rounded-md shadow-sm w-28">
+                                        <span>Sign up</span>
+                                    </button>
+                                )}
+                                {isPending && (
+                                    <button type='submit' className="h-10 text-white bg-gray-600 border rounded-md shadow-sm w-28">
+                                        <button disabled className='flex items-center justify-center w-full'>
+                                            <svg className="w-5 h-5 mr-3 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" ></circle>
+                                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                            </svg>
+                                        </button>
+                                    </button>
+                                )}
                             </div>
                         </form>
                     </div>
