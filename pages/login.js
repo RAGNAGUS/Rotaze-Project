5721e90793e416873d7009a94b96d9e701c7c4e6
Kongsittha Nimking <casnova152@gmail.com>
2022-07-30 17:26:43 +0700

    add anim

--- pages/login.js
+++ pages/login.js
@@ -13,6 +13,8 @@ import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Login() {
 
+    const [isVisible, setIsVisible] = useState(true)
+
     const { loginWithEmailAndPassword, isPending, error } = useLogin();
     const { user } = useAuthContext()
     const router = useRouter()
@@ -32,6 +34,10 @@ export default function Login() {
             router.push('/')
         }
     }, [router, user])
+
+    useEffect(() => {
+        setIsVisible(!isVisible)
+    }, [])
 
     useEffect(() => {
 
