077ba6c8d9437cab354a089ab2b951d2b39666cf
Kongsittha Nimking <casnova152@gmail.com>
2022-10-24 23:37:58 +0700

    fix discover

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -36,10 +36,10 @@ function Discover() {
     return (
         <div className="pt-[73px]  w-full min-h-screen">
             <div className="w-11/12 pt-10 mx-auto">
-                <div className="space-y-3 columns-3xs">
+                <div className="space-y-3 columns-2xs">
                     {documents && documents.map((document, index) => (
                         <div key={index} onClick={() => handleClick(document.value)}>
-                            <div className="relative duration-500 ease-out cursor-pointer hover:scale-105">
+                            {/* <div className="relative duration-500 ease-out cursor-pointer hover:scale-105">
                                 <img
                                     className="bg-white border border-gray-300 rounded-md shadow-sm"
                                     src={document.cover}
@@ -47,6 +47,14 @@ function Discover() {
                                 <div className="absolute bottom-0 w-full px-3 py-3 text-center text-white bg-gray-900 rounded opacity-80">
                                     {document.label}
                                 </div>
+                            </div> */}
+
+                            <div className="relative max-w-sm overflow-hidden duration-500 ease-out rounded shadow-lg cursor-pointer hover:scale-105">
+                                <img src={document.cover} alt="" className="object-cover w-full max-h-48" />
+                                <div className="absolute bottom-0 w-full px-3 py-3 text-center text-white bg-gray-900 rounded opacity-80">
+                                    {document.label}
+                                </div>
+
                             </div>
                         </div>
                     ))}
